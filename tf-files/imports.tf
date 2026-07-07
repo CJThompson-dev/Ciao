5440418f729f891e048cb15394cbb4cff599d1e4
ClarkThompson <c.jthompson9898@live.co.uk>
2026-07-07 20:59:41 +0100

    a lot of things

---
+++ tf-files/imports.tf
@@ -0,0 +1,14 @@
+import {
+  to = aws_instance.hosp
+  id = "i-0d10f38d73d3ea785"
+}
+
+import {
+  to = aws_security_group.hosp_ec2
+  id = "sg-0884e43c8cf150406"
+}
+
+import {
+  to = aws_security_group.hosp_alb
+  id = "sg-04874a6f374d17bf6"
+}
--- tf-files/loadbalancer.tf
+++ tf-files/loadbalancer.tf
@@ -118,7 +118,7 @@ resource "aws_s3_bucket" "ciao-lb-logs" {
   bucket = "ciao-lb-logs-bucket"
 
   tags = {
-    Owner        = "Ciao"
+    Owner = "Ciao"
   }
 }
 
@@ -130,17 +130,152 @@ resource "aws_athena_database" "ciao_athena_table" {
 resource "aws_s3_bucket_policy" "alb_logs_policy" {
   bucket = aws_s3_bucket.ciao-lb-logs.id
   policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Sid    = "AllowALBAccess",
-          Effect = "Allow",
-          Principal = {
-            Service = "logdelivery.elasticloadbalancing.amazonaws.com"
-          },
-          Action = "s3:PutObject",
-          Resource = "arn:aws:s3:::${aws_s3_bucket.ciao-lb-logs.bucket}/*"
-        }
-      ]
-    })
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowALBAccess",
+        Effect = "Allow",
+        Principal = {
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+        },
+        Action   = "s3:PutObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.ciao-lb-logs.bucket}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_security_group" "hosp_alb" {
+  name        = "Load Balancer Security Group Ciao"
+  region      = "eu-west-2"
+  description = "Allows ingress via TCP on port 80 from all sources."
+  vpc_id      = "vpc-080dbb0b7dc86503a"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 80
+    ipv6_cidr_blocks = ["::/0"]
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups  = []
+    self             = false
+    to_port          = 80
+  }]
+  tags = {
+    Owner = "Students"
   }
+}
+
+
+resource "aws_security_group" "hosp_ec2" {
+  name        = "Upstream server security group Ciao"
+  description = "Allows ingress via TCP on port 80 from ALB only"
+  vpc_id      = "vpc-080dbb0b7dc86503a"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.hosp_alb.id] # only from ALB
+  }
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.hosp_alb.id] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "hosp" {
+  ami                                  = "ami-03c24c0dc17d1ba36"
+  associate_public_ip_address          = true
+  availability_zone                    = "eu-west-2b"
+  get_password_data                    = false
+  hibernation                          = false
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t2.micro"
+  key_name                             = "ben"
+  monitoring                           = true
+  region                               = "eu-west-2"
+  source_dest_check                    = true
+  subnet_id                            = "subnet-0e606c290592d4005"
+  tags = {
+    Name  = "HOSP Server Ciao"
+    Owner = "Coaches"
+  }
+  tags_all = {
+    Name  = "HOSP Server Ciao"
+    Owner = "Coaches"
+  }
+  tenancy                = "default"
+  user_data              = "#!/bin/bash\n# Update and install Python 3.9+\nsudo apt-get update -y\nsudo apt-get install -y python3.9\n"
+  vpc_security_group_ids = [aws_security_group.hosp_ec2.id]
+  cpu_options {
+    core_count       = 1
+    threads_per_core = 1
+  }
+  credit_specification {
+    cpu_credits = "standard"
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 1
+    http_tokens                 = "optional"
+    instance_metadata_tags      = "disabled"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 100
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 0
+    volume_size           = 8
+    volume_type           = "gp2"
+  }
+}
+
+resource "aws_launch_template" "hosp" {
+  name_prefix            = "hosp-"
+  image_id               = "ami-03c24c0dc17d1ba36"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.hosp_ec2.id]
+}
+
+resource "aws_autoscaling_group" "hosp" {
+  name                = "hosp-asg"
+  min_size            = 2
+  max_size            = 4
+  desired_capacity    = 2
+  vpc_zone_identifier = ["subnet-09ffb20c4da788637", "subnet-0e606c290592d4005"]
+  # I was going to use AZs here but apparently this is a better way to be exact
+  # It has a very similar to AZs but pulls exact subnets from our elb
+  target_group_arns = [aws_lb_target_group.hosp.arn]
+  launch_template {
+    id      = aws_launch_template.hosp.id
+    version = "$Latest"
+  }
+}
