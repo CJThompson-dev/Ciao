906a334f2705bd997b66e179528187c27485d39b
Alex S <220056072+dal3ks@users.noreply.github.com>
2026-07-07 10:43:29 +0100

    Added load balancer and listener import and s3 bucket details

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,22 @@ import {
   to = aws_lb_target_group.hosp
   id = "arn:aws:elasticloadbalancing:eu-west-2:664047078509:targetgroup/lb-tg-Ciao/e37a9b3b5fd09c50"
 }
+
+import {
+  to = aws_lb.hosp
+  id = "arn:aws:elasticloadbalancing:eu-west-2:664047078509:loadbalancer/app/lb-Ciao/d7fd2dde506cb7dd"
+}
+
+import {
+  to = aws_lb_listener.hosp
+  id = "arn:aws:elasticloadbalancing:eu-west-2:664047078509:listener/app/lb-Ciao/d7fd2dde506cb7dd/16c87ab4c291b355"
+}
+
+terraform {
+  backend "s3" {
+    bucket       = "ciao-tf-backend"
+    key          = "hosp-infra/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+  }
+}
