5e1e3fb89e30e8df02732d39d66bc04144e95879
ClarkThompson <c.jthompson9898@live.co.uk>
2026-07-14 14:49:50 +0100

    zip added

--- tf-files/lambda.tf
+++ tf-files/lambda.tf
@@ -1,9 +1,18 @@
+data "archive_file" "retry_posts_zip" {
+  type        = "zip"
+  source_file = "${path.module}/ciao_handler.py"
+  output_path = "${path.module}/ciao_handler.zip"
+}
+
 resource "aws_lambda_function" "retry_posts" {
   function_name = "ciao-retry-posts"
   role          = aws_iam_role.lambda_retry.arn
   handler       = "ciao_handler.lambda_handler"
   runtime       = "python3.12"
   timeout       = 30
+
+  filename         = data.archive_file.retry_posts_zip.output_path
+  source_code_hash = data.archive_file.retry_posts_zip.output_base64sha256
 
   environment {
     variables = {
